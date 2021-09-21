2fb603addd83dc08cf9b2db479272f8b65f93168
Ajesh01 <ajeshmartin01@gmail.com>
2021-09-21 21:36:31 +0530

    zip file option added

--- ui-jinja/server.py
+++ ui-jinja/server.py
@@ -4,6 +4,7 @@ from werkzeug.utils import  secure_filename
 import json ,os
 import pprint
 import requests
+import zipfile
 
 
 app=Flask(__name__)
@@ -47,7 +48,7 @@ def upload_file():
         # empty file without a filename.
         print(file.filename)
         if file.filename == '':
-            flash('No selected file')
+            # flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -57,9 +58,9 @@ def upload_file():
         f = open(path , "r")
         # words = f.read()
         file_list = f.read().split(delimiter)
-
+    
         app.config[request.remote_addr] = file_list
-        app.config[request.remote_addr+"-file_name"] = path
+        app.config[request.remote_addr+"-file_name"] = filename
 
         return redirect(url_for('home',file_name = filename))
 
@@ -153,10 +154,11 @@ def save_file(tokenized_data, keys_data , words, filename):
 
         res_list.append(data)
         count+=1
+    pattern_data = []
 
     # pprint.pprint(res_list)
     for ner in keys_data:
-
+        # print(keys_data[ner])
         for select in keys_data[ner]:
 
             prev = False
@@ -164,10 +166,13 @@ def save_file(tokenized_data, keys_data , words, filename):
             token_start = None
             token_end = None
             selected_words = select.split()
+            # print(select,selected_words)
             hit = None
+
 
             for word in selected_words:
                 # print(word,keys_data[ner][select] )
+                # print("word", select)
                 for word2 in res_list:
                     # print(word2["start"],"--",word2["text"],"==", word, len(word2["text"]), len(word))
                     # print(res_list)
@@ -177,6 +182,7 @@ def save_file(tokenized_data, keys_data , words, filename):
                         hit = word2
                         if prev == False:
                             token_start = word2["id"]
+                            pattern_data.append({"label": ner ,"pattern":[{"lower": select.lower()}]})
                         else:
                             token_end = word2["id"]
                         prev_value = word2
@@ -196,7 +202,7 @@ def save_file(tokenized_data, keys_data , words, filename):
             
             hit_list.append(data)
 
-    pprint.pprint(hit_list)
+    # pprint.pprint(hit_list)
 
     one_page_data = {
         "text" : words,
@@ -209,29 +215,66 @@ def save_file(tokenized_data, keys_data , words, filename):
         }
     # one_page_data = [json.dumps(one_page_data)]
     # dump_jsonl([one_page_data],"output.jsonl")
-    print(type(one_page_data))
-
     
-    with open(filename, 'a') as outfile:
+
+    pattern_filename = filename.replace(".jsonl","_pattern.jsonl")
+    folder_path = filename.replace(".jsonl","")
+    print(folder_path)
+    try:
+        os.makedirs(folder_path)
+    except FileExistsError:
+        pass
+    with open(os.path.join(folder_path, filename), 'a') as outfile:
         for entry in [one_page_data]:
             json.dump(entry, outfile)
             outfile.write('\n')
     
+    with open(os.path.join(folder_path, pattern_filename), 'a') as outfile:
+        for entry in pattern_data:
+            # print(pattern_data)
+            json.dump(entry, outfile)
+            outfile.write('\n')
+    
+    
 
     return True
 
-@app.route('/export_file', methods=['GET'])
-def return_files():
-    # print(app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl"))
-	
-    # return True
+@app.route('/exportt', methods=['GET'])
+def export_files():
+    filename =app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl")
+    folder_path = filename.replace(".jsonl","")
+    zip_name = filename.replace(".jsonl",".zip")
+    print("hey",zip_name)
+    # return zip_name
 
-		# return send_file(app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl"), attachment_filename='Data.jsonl')
-    file_name = app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl").split("/")[-1]
-    # print(file_name)
-    return_file = send_from_directory(UPLOAD_FOLDER,file_name , as_attachment=True)
-    os.remove(app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl"))
+    zipfolder = zipfile.ZipFile(zip_name,'w', compression = zipfile.ZIP_STORED) # Compression type 
+
+    # zip all the files which are inside in the folder
+    for root,dirs, files in os.walk(folder_path):
+        for file in files:
+            print("file:",file)
+            zipfolder.write(folder_path+"/"+file)
+    zipfolder.close()
+    # return str(True)
+    return_file = send_file(zip_name,
+            mimetype = 'zip',
+            attachment_filename= zip_name,
+            as_attachment = True)
+    os.remove(zip_name)
+
     return return_file
+
+
+
+
+
+
+
+    # file_name = app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl").split("/")[-1]
+    # print(file_name)
+    # return_file = send_from_directory(UPLOAD_FOLDER,file_name , as_attachment=True)
+    # os.remove(app.config[request.remote_addr+"-file_name"].replace(".txt",".jsonl"))
+    # return return_file
 
 	
 
