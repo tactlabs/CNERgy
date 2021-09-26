b8262234afa8bc96a650717e7e9856dd53db0431
Raja CSP Raman <raja.csp@gmail.com>
2021-09-26 03:38:32 -0400

    Local tokenizer introduced

--- annotator/server.py
+++ annotator/server.py
@@ -1,5 +1,6 @@
 import nltk
-
+import spacy
+from spacy.lang.en import English
 from flask import Flask, request
 from flask_cors import cross_origin
 
@@ -7,17 +8,60 @@ from nltk.tokenize.treebank import TreebankWordTokenizer, TreebankWordDetokenize
 
 app = Flask(__name__)
 
+nlp = spacy.load("en_core_web_sm")
+# nlp = English()
 
-@app.route("/tokenize", methods=["POST"])
-@cross_origin()
-def tokenize():
-    text = request.json["text"]
+USE_LOCAL_TOKENIZER = True
+
+def get_tokens_local(text):
+
+    token_list = []
+
+    start_index = 0
+    doc = nlp(text)
+    for sent in doc.sents:
+
+        for token in sent:
+            token_index = (token.i - sent.start)
+            
+            token_length = len(token.text)
+    
+            token_end   = start_index + token_length
+            
+            # print(start_index, token_end)
+
+            current_token_tuple = (start_index, token_end, token.text)
+            token_list.append(current_token_tuple)
+            
+            start_index = start_index + token_length +1
+
+
+    return token_list
+    
+
+def get_tokens_nltk(text):
+
     try:
         spans = list(TreebankWordTokenizer().span_tokenize(text))
     except LookupError:
         nltk.download('punkt')
         spans = list(TreebankWordTokenizer().span_tokenize(text))
-    return {"tokens": [(s[0], s[1], text[s[0]:s[1]]) for s in spans]}
+
+    result = [(s[0], s[1], text[s[0]:s[1]]) for s in spans]
+
+    return result
+
+
+@app.route("/tokenize", methods=["POST"])
+@cross_origin()
+def tokenize():
+    text = request.json["text"]
+
+    if(USE_LOCAL_TOKENIZER):
+        token_list = get_tokens_local(text)
+        return {"tokens": token_list}
+
+    return {"tokens": get_tokens_nltk(text)}
 
 
 @app.route("/detokenize", methods=["POST"])
@@ -26,8 +70,19 @@ def detokenize():
     tokens = request.json["tokens"]
     return {"text": TreebankWordDetokenizer().detokenize(tokens)}
 
-@app.route("/tokenize", methods=["POST"])
+def startpy():
+
+    text = "Welcome to TactLabs. It's awesome!!"
+
+    result = get_tokens_nltk(text)
+    print(result)
+
+    result = get_tokens_local(text)
+    print(result)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5555, debug=True)
+
+    # app.run(host = '0.0.0.0', port = 5555, debug = True)
+
+    startpy()
